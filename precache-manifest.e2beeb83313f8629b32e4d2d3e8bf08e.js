260cce1812841a55c23c07f4b4f1c0fe921563af
baptistemz <mulliez.baptiste@gmail.com>
2019-05-10 16:44:23 +0200

    Updates

---
+++ precache-manifest.e2beeb83313f8629b32e4d2d3e8bf08e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0575e30d003d945694f46543284c8cc0",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "954fac1059c18c3237f7",
+    "url": "/repository-name/static/css/main.83f580e2.chunk.css"
+  },
+  {
+    "revision": "64ab05a9bd107ca70fe0",
+    "url": "/repository-name/static/js/2.27c6c38b.chunk.js"
+  },
+  {
+    "revision": "954fac1059c18c3237f7",
+    "url": "/repository-name/static/js/main.bf7fd71d.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
