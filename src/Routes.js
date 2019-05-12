9d4cb51d5aa023b279ded876852a80d29f41425a
baptistemz <mulliez.baptiste@gmail.com>
2019-05-12 11:16:52 +0200

    feat: 404 custom page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Page404 from './components/404/Page404';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import TermsAndConditions from './components/TermsAndConditions';
 
@@ -8,9 +9,12 @@ class Routes extends Component{
   render(){
     return(
       <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/privacy_policy" component={PrivacyPolicy}/>
-        <Route path="/terms_and_conditions" component={TermsAndConditions}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/privacy_policy" component={PrivacyPolicy}/>
+          <Route path="/terms_and_conditions" component={TermsAndConditions}/>
+          <Route component={Page404} />
+        </Switch>
       </Router>
     )
   }
