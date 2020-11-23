5cf1b340c0dd5e736fad834f548a071b259ddb21
baptistemz <mulliez.baptiste@gmail.com>
2020-11-23 12:29:28 +0100

    Updates

--- precache-manifest.e3fc49717c1bbcbb44ac763697524f76.js
+++ precache-manifest.e3fc49717c1bbcbb44ac763697524f76.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a26f8570f42cd57f306a1d88fcca09ab",
+    "revision": "27de9dda33c00f76a43bd82e35e295cc",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.cb7021a3.chunk.css"
   },
   {
-    "revision": "a98cbfaf071849452ab6",
+    "revision": "0d516e886e4b5c76daf1",
     "url": "/static/css/main.39e3bc12.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0527d041.chunk.js"
   },
   {
-    "revision": "a98cbfaf071849452ab6",
-    "url": "/static/js/main.2ee3fc24.chunk.js"
+    "revision": "0d516e886e4b5c76daf1",
+    "url": "/static/js/main.a4338f54.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
